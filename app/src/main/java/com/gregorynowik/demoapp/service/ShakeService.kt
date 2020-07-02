2aef563744f7a8d3925d178cbb2cafde2a9788e7
gregorynowik <gregorynowik@gmail.com>
2020-07-02 12:17:22 +0200

    added notification when phone is shaken

--- app/src/main/java/com/gregorynowik/demoapp/service/ShakeService.kt
+++ app/src/main/java/com/gregorynowik/demoapp/service/ShakeService.kt
@@ -1,16 +1,24 @@
 package com.gregorynowik.demoapp.service
 
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorManager
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import com.gregorynowik.demoapp.MainActivity
+import com.gregorynowik.demoapp.R
 import com.gregorynowik.demoapp.service.ShakeDetector.OnShakeListener
+import kotlin.random.Random
 
 
 class ShakeService : Service(), OnShakeListener {
+
+    companion object {
+        const val CHANNEL_ID = "demo_app_channel_id"
+    }
 
     // Shake detection
     private var sensorManager: SensorManager? = null
@@ -34,7 +42,66 @@ class ShakeService : Service(), OnShakeListener {
 
 
     override fun onShake(count: Int) {
-        Log.d("GREGAPP", "ON SHAKE")
+        showNotification()
+    }
+
+    private fun showNotification() {
+
+        Log.e("GREGAPP", "show notification")
+
+        // instanciate notification manager
+        val notificationManager: NotificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        // add notification channel if >= Oreo
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        // make intenent pointing to MainActivity
+        val notificationIntent = Intent(baseContext, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            baseContext, 0,
+            notificationIntent, 0
+        )
+
+        val notification =
+            //channel ID is mandatory since Android O
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                Notification.Builder(baseContext, CHANNEL_ID)
+            } else {
+                @Suppress("DEPRECATION")
+                Notification.Builder(baseContext)
+            }
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Your phone was shaken")
+                .setContentText("Open this notification to connect to bluetooth")
+                .build()
+
+        notificationManager.notify(Random.nextInt(), notification)
+
+    }
+
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = "demo app notifications"
+            val description = "Notifications for demo app"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(
+                CHANNEL_ID, name,
+                importance
+            )
+            channel.description = description
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviours after this
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
 }
